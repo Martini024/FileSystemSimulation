f2444c8fcee356caab1734946871c2497a8562ea
martini <zhao435021640@gmail.com>
2018-12-31 18:51:54 +0800

    Initial Commit

---
+++ FileSystemSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FileSystemSimulation
+//
+//  Created by JIN ZHAO on 12/31/18.
+//  Copyright © 2018 JIN ZHAO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
